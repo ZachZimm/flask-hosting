cdd06a3770d8eaa4f531e0570c4a55089387f2fe
Zach <zach@jzimm.com>
2022-02-14 13:10:44 -0800

    /meta now accepts a variable, and if present looks for the corresponding id<var>.json file

--- host_meta.py
+++ host_meta.py
@@ -7,15 +7,21 @@ import logging
 import json
 from flask import Flask, flash, Response, redirect, request, render_template, url_for
 
-metadata = open('static/sean_meta.json')
-metadata = json.load(metadata)
+
 # metadata = "Metadata!"
 
 
 @app.route("/meta/",defaults={'path': ''},methods=["POST","GET"])
 @app.route("/meta/<path:path>",methods=["POST","GET"])
 def meta_route(path):
-    if(request.method == "GET"): return metadata
 
+    filepath = 'static/sean_meta.json'
+    if(path != ''):
+        filepath = 'static/id'+path+'.json'
+    else:'static/sean_meta.json'
+    metadata = open(filepath)
+    metadata = json.load(metadata)
+    # if(request.method == "GET"): return path
+    if(request.method == "GET"): return metadata
 
     return render_template("meta.html", meta=metadata)
