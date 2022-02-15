05e427fffe9810cf180abf9fa546931bd32b042f
Zach <zach@box.zzimm.com>
2022-02-14 16:35:55 -0800

    Fixed a bug that prevented /meta/ from working with HEADS/TAILS mode

--- host_meta.py
+++ host_meta.py
@@ -12,18 +12,21 @@ HEADS_TAILS = True
 @app.route("/meta/",defaults={'path': ''},methods=["POST","GET"])
 @app.route("/meta/<path:path>",methods=["POST","GET"])
 def meta_route(path):
-    path = str(int(path))
     filepath = 'static/sean_meta.json'
 
     if(HEADS_TAILS):
+        if(path == ''):
+            path = "0"
         if((int(path) + 2) % 2 == 0):
             path = "0"
         else:
             path = "1"
 
     if(path != ''):
+        path = str(int(path))
         filepath = 'static/id'+path+'.json'
-    else:'static/sean_meta.json'
+    else:
+        filepath = 'static/sean_meta.json'
     metadata = open(filepath)
     metadata = json.load(metadata)
     # if(request.method == "GET"): return path
